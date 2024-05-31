437c446db25428485cdceeeea54e7de05f6fa4ae
edi <guzmanedi5825@gmail.com>
2024-05-31 15:21:44 -0300

    hasta ahora es solucion con matriz estatica

--- respuesta.h
+++ respuesta.h
@@ -12,4 +12,5 @@ tRespuesta;
 int determinarMejorTiempoYTiempoDuplicado(tRespuesta respuestas[][100],int cantJugadores, int rondaActual,double* mejorTiempo,int* existenDuplicado);
 int calcularPuntaje(char respuestaJugador,char respuestaCorrecta,double tiempoRespuesta,
                     int tiempoLimite,double mejorTiempo,int existeDuplicadoMejorTiempo);
+
 #endif // RESPUESTA_H_INCLUDED
